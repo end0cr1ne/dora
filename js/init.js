10f55124fc487369708c9d50aee8161406755eb9
end0cr1ne <f.warren210@gmail.com>
2016-07-26 00:36:51 +0530

    fix

--- js/init.js
+++ js/init.js
@@ -1,8 +1,69 @@
-(function($){
-  $(function(){
+$(function () {
+  $(".button-collapse").sideNav();
+});
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+$('#fullpage').fullpage({
+  //Navigation
+  menu: '#menu',
+  lockAnchors: false,
+  anchors: ['header', 'research'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['Header', 'Research'],
+  showActiveTooltip: false,
+  slidesNavigation: true,
+  slidesNavPosition: 'bottom',
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  //Scrolling
+  css3: true,
+  scrollingSpeed: 700,
+  autoScrolling: true,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  scrollBar: false,
+  easing: 'easeInOutCubic',
+  easingcss3: 'ease',
+  loopBottom: false,
+  loopTop: false,
+  loopHorizontal: true,
+  continuousVertical: false,
+  normalScrollElements: '#element1, .element2',
+  scrollOverflow: false,
+  scrollOverflowOptions: null,
+  touchSensitivity: 15,
+  normalScrollElementTouchThreshold: 5,
+  bigSectionsDestination: null,
+
+  //Accessibility
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+
+  //Design
+  controlArrows: true,
+  verticalCentered: true,
+  sectionsColor: ['#fff', '#fff'],
+  paddingTop: '3em',
+  paddingBottom: '10px',
+  fixedElements: '#header, .footer',
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+
+  //Custom selectors
+  sectionSelector: '.section',
+  slideSelector: '.slide',
+
+  //events
+  onLeave: function (index, nextIndex, direction) {
+  },
+  afterLoad: function (anchorLink, index) {
+  },
+  afterRender: function () {
+  },
+  afterResize: function () {
+  },
+  afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+  },
+  onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
+  }
+});
